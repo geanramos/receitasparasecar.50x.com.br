9f1dfac9cd8949f02eabd848403e89b8db7f9426
Gean Ramos <geancr10@gmail.com>
2023-05-28 07:47:40 -0300

    dinamico php

---
+++ download.php
@@ -0,0 +1,130 @@
+<!DOCTYPE html>
+<html  >
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="generator" content="Mobirise v5.8.12, mobirise.com">
+  <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1">
+  <link rel="shortcut icon" href="assets-dload/images/mbr-96x96.png" type="image/x-icon">
+  <meta name="description" content="">
+  
+  
+  <title>Receitas para Secar - Home</title>
+  <link rel="stylesheet" href="assets-dload/web/assets/mobirise-icons2/mobirise2.css">
+  <link rel="stylesheet" href="assets-dload/bootstrap/css/bootstrap.min.css">
+  <link rel="stylesheet" href="assets-dload/bootstrap/css/bootstrap-grid.min.css">
+  <link rel="stylesheet" href="assets-dload/bootstrap/css/bootstrap-reboot.min.css">
+  <link rel="stylesheet" href="assets-dload/parallax/jarallax.css">
+  <link rel="stylesheet" href="assets-dload/dropdown/css/style.css">
+  <link rel="stylesheet" href="assets-dload/socicon/css/styles.css">
+  <link rel="stylesheet" href="assets-dload/theme/css/style.css">
+  <link rel="preload" href="https://fonts.googleapis.com/css?family=Jost:100,200,300,400,500,600,700,800,900,100i,200i,300i,400i,500i,600i,700i,800i,900i&display=swap" as="style" onload="this.onload=null;this.rel='stylesheet'">
+  <noscript><link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Jost:100,200,300,400,500,600,700,800,900,100i,200i,300i,400i,500i,600i,700i,800i,900i&display=swap"></noscript>
+  <link rel="preload" as="style" href="assets-dload/mobirise/css/mbr-additional.css?v2">
+  <link rel="stylesheet" href="assets-dload/mobirise/css/mbr-additional.css?v2" type="text/css">
+
+  
+  
+  
+</head>
+<body>
+  
+  <section data-bs-version="5.1" class="menu menu2 cid-tFvVQYQPXC" once="menu" id="menu2-k">
+    
+    <nav class="navbar navbar-dropdown navbar-fixed-top navbar-expand-lg">
+        <div class="container">
+            <div class="navbar-brand">
+                <span class="navbar-logo">
+                    <a href="donwload.html#top">
+                        <img src="assets-dload/images/mbr-96x96.png" alt="Receitas para Secar" style="height: 3rem;">
+                    </a>
+                </span>
+                <span class="navbar-caption-wrap"><a class="navbar-caption text-black text-primary display-7" href="donwload.html#top">Receitas para Secar</a></span>
+            </div>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-bs-toggle="collapse" data-target="#navbarSupportedContent" data-bs-target="#navbarSupportedContent" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                <div class="hamburger">
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                </div>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav nav-dropdown" data-app-modern-menu="true"><li class="nav-item"><a class="nav-link link text-black text-primary display-4" href="donwload.html#header7-o">
+                            About us</a></li>
+                    <li class="nav-item"><a class="nav-link link text-black text-primary display-4" href="donwload.html#header7-o">
+                            Services</a></li>
+                    <li class="nav-item"><a class="nav-link link text-black text-primary display-4" href="donwload.html#header7-o">Contacts</a>
+                    </li></ul>
+                
+                <div class="navbar-buttons mbr-section-btn"><a class="btn btn-success display-4" href="donwload.html#header7-o"><span class="mobi-mbri mobi-mbri-download mbr-iconfont mbr-iconfont-btn"></span>
+                        DOWNLOAD</a></div>
+            </div>
+        </div>
+    </nav>
+</section>
+
+<section data-bs-version="5.1" class="header10 cid-tFvW36TgSl mbr-parallax-background" id="header10-l">
+
+    
+
+    
+    <div class="mbr-overlay" style="opacity: 0.8; background-color: rgb(255, 255, 255);"></div>
+
+    <div class="align-center container-fluid">
+        <div class="row justify-content-center">
+            <div class="col-md-12 col-lg-9">
+                <h1 class="mbr-section-title mbr-fonts-style mb-3 display-1"><strong>Parabéns!</strong><br><em>Compra&nbsp;realizada com sucesso.</em></h1>
+                <p class="mbr-text mbr-fonts-style display-7">Você só precisa clicar no Botão abaixo, e baixar seu E-book.</p>
+                <div class="mbr-section-btn mt-3"><a class="btn btn-success display-4" href="donwload.html#header7-o"><span class="mobi-mbri mobi-mbri-to-local-drive mbr-iconfont mbr-iconfont-btn"></span>CLIQUE AQUI PARA BAIXA O E-BOOK</a></div>
+                <div class="image-wrap mt-4">
+                    <img src="https://receitasparasecar.50x.com.br/assets-dload/images/biasantos.net-1640x924.png" alt="Receitas para Secar" title="">
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section data-bs-version="5.1" class="header7 cid-tFvYGdTSn3 mbr-fullscreen" id="header7-o">
+
+    
+
+    
+    
+
+    <div class="text-right container">
+        <div class="row justify-content-end">
+            <div class="col-12 col-lg-5">
+                <h1 class="mbr-section-title mbr-fonts-style mb-3 display-1"><strong>Duvidas!</strong></h1>
+                
+                <p class="mbr-text mbr-fonts-style display-5">Chame no WhatsApp.</p>
+                <div class="mbr-section-btn mt-3"><?php
+$payment_id = $_GET["payment_id"];
+$external_reference = $_GET["external_reference"];
+echo "<a class="btn btn-success display-7\" href=\"https://api.whatsapp.com/send?phone=5591982510830&text=Detalhes%20do%20produto%0AEbook%20-%20Metodo%20Secar%20Barriga%0A%0ANumero%20de%20opera%C3%A7%C3%A3o:%20$payment_id%0ACodigo%20do%20Produto:%20$external_reference%0AAtraves%20do:%20Mercadopago%20com%20Representa%C3%A7%C3%B5es%20Ltda.\" target=\"_blank\">";
+?><span class="socicon socicon-whatsapp mbr-iconfont mbr-iconfont-btn"></span>FALE CONOSCO!</a></div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section data-bs-version="5.1" class="footer7 cid-tFvWK52HLk" once="footers" id="footer7-n">
+
+    
+
+    
+
+    <div class="container">
+        <div class="media-container-row align-center mbr-white">
+            <div class="col-12">
+                <p class="mbr-text mb-0 mbr-fonts-style display-7">
+                    © Copyright 2023 Receitas para Secar - All Rights Reserved
+                </p>
+            </div>
+        </div>
+    </div>
+</section><script src="https://receitasparasecar.50x.com.br/assets-dload/bootstrap/js/bootstrap.bundle.min.js"></script>  <script src="https://receitasparasecar.50x.com.br/assets-dload/parallax/jarallax.js"></script>  <script src="https://receitasparasecar.50x.com.br/assets-dload/smoothscroll/smooth-scroll.js"></script>  <script src="https://receitasparasecar.50x.com.br/assets-dload/ytplayer/index.js"></script>  <script src="https://receitasparasecar.50x.com.br/assets-dload/dropdown/js/navbar-dropdown.js"></script>  <script src="https://receitasparasecar.50x.com.br/assets-dload/theme/js/script.js"></script>  
+  
+  
+</body>
+</html>
